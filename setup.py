3898d2c7e7dbdfb9741b2950a70ce4a08474f290
AlloJS <eventorganization1@gmail.com>
2024-06-12 19:49:47 +0200

    Aggiunta del setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='createPassGenerator',
+    version='1.0.0',
+    description='Una libreria per generare password sicure e gestire le password associate.',
+    author=['Angelo Bellanca','Lorenzo Malferrari'],
+    author_email='',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
